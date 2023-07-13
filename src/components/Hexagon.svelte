13f1ebd47c119244e1902f97f5ffa2b77159f910
Cromski <jakobrhenriksen@outlook.com>
2023-07-13 17:48:53 +0200

    made it a component with dynamic values; started doing some clickable stuff, that doesn't work correctly yet :s

--- src/components/Hexagon.svelte
+++ src/components/Hexagon.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+    export let right: number
+    export let mt: number
     export let text: string;
     export let imgPath: string;
+    export let value: number;
+
+    const bing = () => console.log(value)
+
 </script>
 
-<div class="relative mask mask-hexagon w-40 aspect-square">
-    <div style={`background-image: url('${imgPath}')`} class="absolute w-full h-full bg-cover" >
-
-    </div>
-    
-    <h1 class=" absolute bg-black bg-opacity-50 text-white text-center bottom-0 h-[40%] w-full">{text}</h1>
+<div style={`right: -${right}px; margin-top: -${mt}px`} class={`relative mask mask-hexagon w-40 aspect-square hover:scale-[120%] hover:z-20`}>
+    <div style={`background-image: url('${imgPath}')`} class="absolute w-full h-full bg-cover overflow-hidden" />
+    <h1 class=" absolute bg-black bg-opacity-50 text-white overflow-hidden text-center bottom-0 h-[40%] w-full">{text}</h1>
 </div>
-<!-- <img class="w-full h-full" src="./images/karate.jpg" alt="asdffffff"> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
     import Hexagon from '../components/Hexagon.svelte';
+
+  
 </script>
 
-<Hexagon text={"Aktiviteter"} imgPath={"/karate.jpg"}/>
+<div class=" w-[320px] grid grid-cols-2 mt-20">
+
+    <Hexagon right={160} mt={0} text="Aktiviteter" imgPath="/karate.jpg" value={1} />
+    <Hexagon right={160} mt={0} text="Aktiviteter" imgPath="/karate.jpg" value={2} />
+    <Hexagon right={80} mt={24} text="Aktiviteter" imgPath="/karate.jpg" value={3} />
+    <Hexagon right={80} mt={24} text="Aktiviteter" imgPath="/karate.jpg" value={4} />
+    <Hexagon right={0} mt={24} text="Aktiviteter" imgPath="/karate.jpg"  value={5} />
+    <Hexagon right={0} mt={24} text="Aktiviteter" imgPath="/karate.jpg"  value={6} />
+
+</div>
+
+
