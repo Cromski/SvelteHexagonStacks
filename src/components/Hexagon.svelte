daf4582a0be93d8c5680c0d86243a80e5133f8f9
Cromski <jakobrhenriksen@outlook.com>
2023-07-13 23:33:08 +0200

    after clip path now works ish, still hella ugly lmao 420 xd xd

--- src/components/Hexagon.svelte
+++ src/components/Hexagon.svelte
@@ -7,9 +7,25 @@
 
     const bing = () => console.log(value)
 
+    let hover = false;
+
+    $: console.log("hover: " + hover)
+
 </script>
 
-<div style={`right: -${right}px; margin-top: -${mt}px`} class={`relative mask mask-hexagon w-40 aspect-square hover:scale-[120%] hover:z-20`}>
-    <div style={`background-image: url('${imgPath}')`} class="absolute w-full h-full bg-cover overflow-hidden" />
-    <h1 class=" absolute bg-black bg-opacity-50 text-white overflow-hidden text-center bottom-0 h-[40%] w-full">{text}</h1>
+
+
+<div style={`right: -${right}px; clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%); margin-top: -${mt}px; ${hover ? 'scale: 110%; z-index: 80':'z-index:0'}
+    transition: scale 0.5s
+    `} 
+     class={` relative mask mask-hexagon w-40 aspect-square`}>
+
+    <div on:mouseenter={() => hover = true} on:mouseleave={() => hover = false} class="absolute w-[20%] left-[40%] h-[98%] top-[1%]  bg-cover z-50" /> 
+    <div on:mouseenter={() => hover = true} on:mouseleave={() => hover = false} class="absolute w-[35%] left-[32%] h-[90%] top-[5%]  bg-cover z-50" /> 
+    <div on:mouseenter={() => hover = true} on:mouseleave={() => hover = false} class="absolute w-[55%] left-[22.5%] h-[80%] top-[10%] bg-cover z-50" /> 
+    <div on:mouseenter={() => hover = true} on:mouseleave={() => hover = false} class="absolute w-[68%] left-[15%] h-[70%] top-[15%]  bg-cover z-50" /> 
+    <div on:mouseenter={() => hover = true} on:mouseleave={() => hover = false} class="absolute w-[80%] left-[10%] h-[60%] top-[20%]  bg-cover z-50" /> 
+    <div on:mouseenter={() => hover = true} on:mouseleave={() => hover = false} class="absolute w-[90%] left-[5%] h-1/2 top-[25%]  bg-cover z-50" /> 
+    <div style={`background-image: url('${imgPath}')`} class="absolute w-full h-full bg-cover " />
+    <h1 style={`${hover ? 'height: 45%' : 'height: 40%'}; transition: height 0.2s`} class=" absolute bg-black bg-opacity-50 text-white text-center bottom-0 w-full">{text}</h1>
 </div>
